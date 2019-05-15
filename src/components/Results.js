a90dde82c9c1d62a4427af85bcaab0e04cac35b8
Redmond Chan <redmondchan92@gmail.com>
2019-05-15 00:11:54 -0400

    parsed patient id to integer so sortResults function can work properly

--- src/components/Results.js
+++ src/components/Results.js
@@ -11,23 +11,26 @@ class Results extends React.Component{
         result.push(`{"patientId": ${obj.patientId}, "index": []}`)
       }
     })
-
     newResult = result.map(obj => JSON.parse(obj))
+    console.log("before newResult", result, newResult, array)
 
     for(let i=0; i < array.length; i++){
       for(let j=0; j<newResult.length; j++){
         if(newResult[j].patientId === array[i].patientId){
+          console.log("index", array[i].index[0])
           newResult[j].index.push(array[i].index[0])
         }else {
 
         }
       }
     }
+    console.log("newResult", newResult)
     return newResult
   }
 
   render(){
     let sortedArray = this.sortResults(this.props.results)
+    console.log("sortedArray", sortedArray)
     let sortedDivs = sortedArray.map(obj => <div>Patient {obj.patientId}: Found sequence at {obj.index.join(', ')}</div>)
     return(
       <div>
--- src/containers/container.js
+++ src/containers/container.js
@@ -43,41 +43,39 @@ class Container extends React.Component{
   }
 
   insertionOrDeletion = (control, array) => {
-    console.log("insertion deletion", control, array)
     let results = []
-for(let i=0; i < array.length; i++){
-  if(array[i].dna.length > control.length){
-    let n = 0
-    for(let j=0; j<array[i].dna.length; j++){
-      let x = j + n
-      if(array[i].dna[x] !== control.charAt(j)){
-        n++
-        console.log(n, "charAt", j, x, array[i].dna[x], array[i].patientId, control.charAt(j))
-        if(array[i].dna[x] !== undefined){
-          results.push({id: array[i].patientId, mutation: "insertion", index: [x]})
+    for(let i=0; i < array.length; i++){
+      if(array[i].dna.length > control.length){
+        let n = 0
+        for(let j=0; j<array[i].dna.length; j++){
+          let x = j + n
+          if(array[i].dna[x] !== control.charAt(j)){
+            n++
+            if(array[i].dna[x] !== undefined){
+              results.push({patientId: parseInt(array[i].patientId), mutation: "insertion", index: [x]})
+            }
+          }
+        }
+      }
+      if(array[i].dna.length < control.length){
+        let n = 0
+        if(control.includes(array[i].dna)){
+         results.push({patientId: parseInt(array[i].patientId), mutation: "deletion", index: [8]})
+        } else {
+          for(let j=0; j<control.length; j++){
+          let x = j + n
+          if(array[i].dna[j] !== control.charAt(x)){
+            n++
+            if(array[i].dna[j] !== undefined){
+              results.push({patientId: parseInt(array[i].patientId), mutation: "deletion", index: [j]})
+            }
+          }
+        }
         }
       }
     }
-  }
-  if(array[i].dna.length < control.length){
-    let n = 0
-    if(control.includes(array[i].dna)){
-     results.push({id: array[i].patientId, mutation: "deletion", index: [8]})
-    } else {
-      for(let j=0; j<control.length; j++){
-      let x = j + n
-      if(array[i].dna[j] !== control.charAt(x)){
-        n++
-        if(array[i].dna[j] !== undefined){
-          results.push({id: array[i].patientId, mutation: "deletion", index: [j]})
-        }
-      }
-    }
-    }
-  }
-}
-console.log("insert", results)
-return results
+    console.log("insert", results)
+    return results
   }
 
   // <Results results={this.state.insertionDeletion ? this.insertionOrDeletion(this.state.seq, this.state.results) : this.searchForSeq(this.state.results, this.state.seq)}/>
@@ -90,6 +88,7 @@ return results
         Hi
         <SeqForm csvToJson={this.csvToJson}/>
         <InsertionDeletionForm csvToJson={this.csvToJson}/>
+        <Results results={this.state.insertionDeletion ? this.insertionOrDeletion(this.state.seq, this.state.results) : this.searchForSeq(this.state.results, this.state.seq)}/>
       </div>
     )
   }
