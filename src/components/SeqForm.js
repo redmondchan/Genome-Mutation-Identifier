d8b36b61cfcc15652802b29847932c7811662245
Redmond Chan <redmondchan92@gmail.com>
2019-05-16 23:28:33 -0400

    changed to grid component from material to style the form

--- src/components/SeqForm.js
+++ src/components/SeqForm.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import Card from '@material-ui/core/Card'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
 
 
 class SeqForm extends React.Component{
@@ -25,22 +26,28 @@ class SeqForm extends React.Component{
     e.preventDefault()
     this.props.csvToJson(this.state.csv, this.state.seq, this.state.insertionDeletion)
   }
+  // <input label="CSV File" variant="outlined" type="file" name="csv" onChange={this.handleChangeFile}/>
 
   render(){
     return(
-      <Card style={{float: 'left', width: '44%'}}>
-        <form onSubmit={this.handleSubmit}>
-          <TextField label="DNA Sequence" variant="outlined" type="textarea" name="seq" onChange={this.handleChange}/>
-          <input label="CSV File" variant="outlined" type="file" name="csv" onChange={this.handleChangeFile}/>
-          <input className="hide-button" id="contained-button-file" type="file"/>
-          <label htmlFor="contained-button-file">
-          <Button variant="contained" component="span" onChange={this.handleChangeFile}>
-          Upload
-          </Button>
-          </label>
-          <button>Search</button>
-        </form>
-      </Card>
+      <Grid item xs={6}>
+        <Paper>
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-input">
+              <TextField label="DNA Sequence" variant="outlined" type="textarea" name="seq" onChange={this.handleChange}/>
+            </div>
+            <div className="form-input">
+              <input className="hide-button" id="contained-button-file" type="file" name="csv" onChange={this.handleChangeFile}/>
+              <label htmlFor="contained-button-file">
+              <Button variant="contained" component="span">Upload CSV File</Button>
+              </label>
+            </div>
+            <div className="form-input">
+              <button>Search</button>
+            </div>
+          </form>
+        </Paper>
+      </Grid>
     )
   }
 }
